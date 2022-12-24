d3d4a14ad85aee67743e152d5c409f9758ba6b5b
litvinova08 <ma.litvinova08@gmail.com>
2022-12-24 14:15:21 +1100

    an image added to README.md

--- scripts/booksHandler.js
+++ scripts/booksHandler.js
@@ -1,14 +1,21 @@
 import { GOOGLE_URL } from "./variables.js";
 import { createCard } from "./card.js";
 
+
+
+//function to fetch book as per search request
+export const getBooks = async (searchTerms) => {
+  const response = await fetch(`${GOOGLE_URL}${searchTerms}`);
+  const books = await response.json();
+  return await books.items;
+};
+
 //function to get a book object with some information to display
 export const getBookData = (book) => {
   let checkAuthors = book.volumeInfo.authors;
   if (checkAuthors.length > 2) {
     checkAuthors = `${checkAuthors[0]} et al.`;
   }
-
-  // https://loremflickr.com/g/300/200/dog - image placeholder
 
   return {
     image: (book.volumeInfo.imageLinks.thumbnail ?? "https://loremflickr.com/g/300/200/dog" ), // test this line
@@ -19,13 +26,6 @@ export const getBookData = (book) => {
     publisher: book.volumeInfo.publisher,
     languages: book.volumeInfo.language,
   };
-};
-
-//function to fetch book as per search request
-export const getBooks = async (searchTerms) => {
-  const response = await fetch(`${GOOGLE_URL}${searchTerms}`);
-  const books = await response.json();
-  return await books.items;
 };
 
 //function to rendeer a grid of books
